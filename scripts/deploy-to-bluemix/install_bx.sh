abddff59b4f2a61865dc4017176848b73080dae9
AnthonyAmanse <joe.amanse@obf.ateneo.edu>
2017-10-06 10:26:55 -0700

    Update toolchain

--- scripts/deploy-to-bluemix/install_bx.sh
+++ scripts/deploy-to-bluemix/install_bx.sh
@@ -1,10 +1,5 @@
 #!/bin/bash -e
 
-if [[ -z "$BLUEMIX_AUTH" ]]; then
-    echo -e "\033[0;33mPull Request detected; not installing extra software.\033[0m"
-    exit 0
-fi
-
 echo "Installing Bluemix CLI"
 curl -L http://public.dhe.ibm.com/cloud/bluemix/cli/bluemix-cli/latest/Bluemix_CLI_amd64.tar.gz > Bluemix_CLI.tar.gz
 tar -xvf Bluemix_CLI.tar.gz
